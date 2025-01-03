64d063c94b722c14968fa6745e56a0f4b8903c20
rakaaaaaarioo <101270597+rakaaaaaarioo@users.noreply.github.com>
2025-01-03 22:05:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import pickle
 
 st.title("Aplikasi Klastering Wine Dengan KMeans")
 
-# Muat data
 wine = pd.read_csv('wine-clustering.csv')
 x = wine.iloc[:]
 
@@ -15,12 +14,12 @@ st.header("Isi data set")
 st.write(wine)
 st.write("Dimensi data:", wine.shape)
 
-# Proses KMeans
 inertias = []
 k_range = range(1, 11)
 
 progress = st.progress(0)  # Progress bar
 
+#Tampilan Nilai K terbaik
 for i, k in enumerate(k_range):
     km = KMeans(n_clusters=k, random_state=42).fit(x)
     inertias.append(km.inertia_)
@@ -39,22 +38,20 @@ st.sidebar.subheader("Nilai Jumlah K")
 clust = st.sidebar.slider("Pilih Jumlah Klaster :", 2, 10, 3, 1)
 
 def k_means(n_clust):
-    kmeans = KMeans(n_clusters=n_clust, random_state=42)  # Inisialisasi KMeans
-    y_kmeans = kmeans.fit_predict(x)  # Fit dan prediksi kluster
+    kmeans = KMeans(n_clusters=n_clust, random_state=42)
+    y_kmeans = kmeans.fit_predict(x) 
 
-    # Membuat plot
     plt.figure(figsize=(12, 8))
     plt.scatter(x.iloc[y_kmeans == 0, 0], x.iloc[y_kmeans == 0, 1], s=100, c='red', label='Cluster 1')
     plt.scatter(x.iloc[y_kmeans == 1, 0], x.iloc[y_kmeans == 1, 1], s=100, c='blue', label='Cluster 2')
     plt.scatter(x.iloc[y_kmeans == 2, 0], x.iloc[y_kmeans == 2, 1], s=100, c='green', label='Cluster 3')
     plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='black', label='Centroids')
     plt.title('Clusters of Wine')
-    plt.xlabel('Feature 1')  # Ganti dengan nama kolom yang sesuai
-    plt.ylabel('Feature 2')  # Ganti dengan nama kolom yang sesuai
+    plt.xlabel('Index (Baris)')
+    plt.ylabel('Value')
     plt.legend(loc='upper right')
 
     st.header('Cluster Plot')
     st.pyplot(plt)  # Tampilkan plot
-
-# Panggil fungsi k_means untuk melakukan klastering
+ 
 k_means(clust)
