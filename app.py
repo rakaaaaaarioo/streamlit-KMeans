b50c504b08790496f7b86a9a16f4c078ed2353d2
rakaaaaaarioo <101270597+rakaaaaaarioo@users.noreply.github.com>
2025-01-03 22:09:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from sklearn.cluster import KMeans
 import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
-import pickle
+import matplotlib.cm as cm
 
 st.title("Aplikasi Klastering Wine Dengan KMeans")
 
@@ -38,20 +38,27 @@ st.sidebar.subheader("Nilai Jumlah K")
 clust = st.sidebar.slider("Pilih Jumlah Klaster :", 2, 10, 3, 1)
 
 def k_means(n_clust):
-    kmeans = KMeans(n_clusters=n_clust, random_state=42)
-    y_kmeans = kmeans.fit_predict(x) 
+    kmeans = KMeans(n_clusters=n_clust, random_state=42) 
+    y_kmeans = kmeans.fit_predict(x)
 
     plt.figure(figsize=(12, 8))
-    plt.scatter(x.iloc[y_kmeans == 0, 0], x.iloc[y_kmeans == 0, 1], s=100, c='red', label='Cluster 1')
-    plt.scatter(x.iloc[y_kmeans == 1, 0], x.iloc[y_kmeans == 1, 1], s=100, c='blue', label='Cluster 2')
-    plt.scatter(x.iloc[y_kmeans == 2, 0], x.iloc[y_kmeans == 2, 1], s=100, c='green', label='Cluster 3')
-    plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='black', label='Centroids')
+    
+    # Mendapatkan palet warna berdasarkan jumlah kluster
+    colors = cm.get_cmap('tab10', n_clust)  # Menggunakan colormap 'tab10'
+    
+    for i in range(n_clust):
+        plt.scatter(x.iloc[y_kmeans == i, 0], x.iloc[y_kmeans == i, 1], 
+                    s=100, c=colors(i), label=f'Cluster {i + 1}')
+
+    plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], 
+                s=300, c='black', label='Centroids')
+    
     plt.title('Clusters of Wine')
     plt.xlabel('Index (Baris)')
     plt.ylabel('Value')
     plt.legend(loc='upper right')
 
     st.header('Cluster Plot')
-    st.pyplot(plt)  # Tampilkan plot
- 
+    st.pyplot(plt)
+
 k_means(clust)
