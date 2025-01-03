22d9745258999ad99af34592bbe0cbe22833d19a
rakaaaaaarioo <101270597+rakaaaaaarioo@users.noreply.github.com>
2025-01-03 21:43:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,30 +2,32 @@ import numpy as np
 from sklearn.cluster import KMeans
 import streamlit as st
 import matplotlib.pyplot as plt
-from matplotlib.patches import Ellipse
-import matplotlib.transforms as transforms
 import pandas as pd
 
 st.title("Aplikasi Klastering Wine Dengan KMeans")
 
 wine = pd.read_csv('wine-clustering.csv')
-
 x = wine.iloc[:]
 
 st.header("Isi data set")
 st.write(wine)
+st.write("Dimensi data:", wine.shape)
 
+# Proses KMeans
 inertias = []
-k_range = range (1,11)
-for k in k_range:
-    km = KMeans(n_clusters=k).fit(x)
+k_range = range(1, 11)
+
+progress = st.progress(0)  # Progress bar
+
+for i, k in enumerate(k_range):
+    km = KMeans(n_clusters=k, random_state=42).fit(x)
     inertias.append(km.inertia_)
+    progress.progress((i + 1) / len(k_range))
 
+# Plot hasil
 plt.figure(figsize=(12, 8))
-plt.xlabel('nilai k')
-plt.ylabel('sum of errors')
-plt.plot(k_range,inertias)
+plt.xlabel('Nilai k')
+plt.ylabel('Sum of Squared Errors')
+plt.plot(k_range, inertias, marker='o')
 plt.grid()
-
-st.set_option('deprecation.showPyplotGlobalUse', False)
-inertiass = st.pyplot()
+st.pyplot(plt) 
